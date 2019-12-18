575f61586427acae0bce1c307d76c08608cebcd9
goreleaserbot <goreleaser@carlosbecker.com>
2019-12-18 16:07:42 +0000

    Brew formula update for cheat version v1.0.0

--- cheat.rb
+++ cheat.rb
@@ -2,9 +2,9 @@
 class Cheat < Formula
   desc "A fast and flexible cheatsheet manager built with Go"
   homepage "https://github.com/darrikonn/cheat"
-  url "https://github.com/darrikonn/cheat/releases/download/v0.4.0/cheat-v0.4.0-macOS-64bit.tar.gz"
-  version "0.4.0"
-  sha256 "b7e52b356ce8a1325268ac20d3a974964a8167edc15dcad7e4c9280a383e0151"
+  url "https://github.com/darrikonn/cheat/releases/download/v1.0.0/cheat-v1.0.0-macOS-64bit.tar.gz"
+  version "1.0.0"
+  sha256 "c1f03dc84d7665fc983983411f02f7c3a8e6748bc8d84d4b14afeb45a0b59e2c"
 
   def install
     bin.install "cheat"
